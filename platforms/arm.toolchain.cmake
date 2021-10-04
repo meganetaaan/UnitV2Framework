5b29e43687487b009685b2ac2755ea23897e1a26
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-10-04 04:17:39 +0000

    Add compile option for x64

---
+++ platforms/arm.toolchain.cmake
@@ -0,0 +1,6 @@
+# set compiler
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+
+SET(NCNN_INSTALL_DIR /external/ncnn/install/arm)
+SET(ZBAR_INSTALL_DIR /external/zbar/install/arm)
---
+++ platforms/x64.toolchain.cmake
@@ -0,0 +1,3 @@
+# Nothing necessary for now
+SET(NCNN_INSTALL_DIR /external/ncnn/install/x64)
+SET(ZBAR_INSTALL_DIR /external/zbar-0.10/build/x64)
