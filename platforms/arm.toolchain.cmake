a46eec0f361ee5adc15b04a5bd8f50da7c3a39d7
Shinya Ishikawa <ishikawa@rt-net.jp>
2021-10-14 02:48:16 +0000

    Fix zbar install directory

--- platforms/arm.toolchain.cmake
+++ platforms/arm.toolchain.cmake
@@ -3,4 +3,4 @@ set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
 set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
 
 SET(NCNN_INSTALL_DIR /external/ncnn/install/arm)
-SET(ZBAR_INSTALL_DIR /external/zbar/install/arm)
+SET(ZBAR_INSTALL_DIR /external/zbar-0.10/build/arm)
